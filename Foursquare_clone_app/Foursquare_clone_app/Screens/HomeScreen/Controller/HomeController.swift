08a54801b67435dace06a1f802ef5fba9a2ac027
Максим <maks.mihalchuk.2000@gmail.com>
2020-12-14 17:27:54 +0200

    project architecture

---
+++ Foursquare_clone_app/Foursquare_clone_app/Screens/HomeScreen/Controller/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Foursquare_clone_app
+//
+//  Created by maks on 14.12.2020.
+//  Copyright © 2020 maks. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
