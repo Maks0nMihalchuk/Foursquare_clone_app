427d2892a3e9c72d2a8101d8504a6d5773abab8c
Максим <maks.mihalchuk.2000@gmail.com>
2021-01-28 10:52:35 +0200

    deleted getPhoto function

--- Foursquare_clone_app/Foursquare_clone_app/Managers/NetworkManager/NetworkManager.swift
+++ Foursquare_clone_app/Foursquare_clone_app/Managers/NetworkManager/NetworkManager.swift
@@ -211,36 +211,7 @@ class NetworkManager {
                 completion(StorageManager.shared.getUserLists(), true)
             } catch {
                 completion(nil, false)
-
             }
-        }
-    }
-
-    func getPhoto(prefix: String?, suffix: String?, completion: @escaping (Data?) -> Void) {
-        guard let pref = prefix,
-              let suff = suffix
-        else {
-            completion(nil)
-            return
-        }
-
-        let urlString = "\(pref)500x500\(suff)"
-
-        guard let url = URL(string: urlString) else {
-            completion(nil)
-            return
-        }
-
-        makeDataTaskRequest(with: url) { (data, error) in
-            if error != nil {
-                completion(nil)
-                return
-            }
-            guard let data = data else {
-                completion(nil)
-                return
-            }
-            completion(data)
         }
     }
 
